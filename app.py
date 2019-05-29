9bc61bd9c236533f7148bccfaaf2150261b8aac1
Juliet <jewlz19@hotmail.co.uk>
2019-05-29 19:32:21 +0000

    Added first recipe

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'Recipe_Cookbook'
+app.config["MONGO_URI"] = 'mongodb+srv://Julietisstudent:Julietisstudent@recipecookbook-qpag8.mongodb.net/Recipe_Cookbook?retryWrites=true'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.Recipes.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
