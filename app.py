a0e72c164e7b1779f967be6083e141c365ff6418
Juliet <jewlz19@hotmail.co.uk>
2019-05-30 15:29:52 +0000

    Added form and menu items

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ mongo = PyMongo(app)
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.Recipes.find())
     
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html',
+    RecipeCategories=mongo.db.RecipeCategories.find())
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
